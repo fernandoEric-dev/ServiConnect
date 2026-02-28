a2b360d035b3cc0ed1174001e835636f9f3faef7
Fernando <fernandoeric.feds8@gmail.com>
2026-02-28 01:00:04 -0300

    Update conexao.php

--- backend/conexao.php
+++ backend/conexao.php
@@ -1,17 +1,9 @@
 <?php
-$is_local = ($_SERVER['HTTP_HOST'] == 'localhost' || $_SERVER['HTTP_HOST'] == '127.0.0.1');
-
-if ($is_local) {
-    $host = 'localhost';
-    $db   = 'serviconnect_banco';
-    $user = 'root';
-    $pass = '';
-} else {
-    $host = 'sql105.infinityfree.com';
-    $db   = 'if0_41205929_serviconnecct_banco '; 
-    $user = 'if0_41205929'; 
-    $pass = 'legalizadO1'; 
-}
+// DADOS EXCLUSIVOS DO INFINITYFREE
+$host = 'sql105.infinityfree.com';
+$db   = 'if0_41205929_serviconnecct_banco'; // Espaço final removido!
+$user = 'if0_41205929'; 
+$pass = 'legalizadO1'; 
 
 $dsn = "mysql:host=$host;dbname=$db;charset=utf8mb4";
 $options = [
